aeb9482fbc1b0d986fa123dc47360a559f8c9cdb
gabriell74 <gabrielvivaldi07@gmail.com>
2025-12-19 18:53:43 +0700

    Pembaruan sintaks  pewarnaan deprecated menggunakan `withValues` pada login required dialog

--- lib/core/widgets/login_required_dialog.dart
+++ lib/core/widgets/login_required_dialog.dart
@@ -19,7 +19,7 @@ class LoginRequiredDialog extends StatelessWidget {
           borderRadius: BorderRadius.circular(24),
           boxShadow: [
             BoxShadow(
-              color: Colors.black.withOpacity(0.1),
+              color: Colors.black.withValues(alpha: 0.1),
               blurRadius: 20,
               offset: const Offset(0, 10),
             ),
@@ -32,7 +32,7 @@ class LoginRequiredDialog extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
-                color: const Color(0xFF55C173).withOpacity(0.1),
+                color: const Color(0xFF55C173).withValues(alpha: 0.1),
                 shape: BoxShape.circle,
               ),
               child: const Icon(
